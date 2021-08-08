ed2148ec5bf3592589b64c16c9aead2581f3599d
Teimur Gasanov <teymurgg321@gmail.com>
2021-08-08 11:38:03 +0600

    Add docs GH job

---
+++ bin/generate-readme.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+const fs = require("fs")
+const path = require("path")
+
+const getPatterns = () => {
+  const dirPath = path.resolve(__dirname)
+  const patternsRelativePath = "../src/patterns"
+  const patternsPath = path.join(dirPath, patternsRelativePath)
+
+  return fs.readdirSync(patternsPath).map((patternName) => {
+    const problemsPath = path.join(patternsPath, patternName)
+    const problems = fs.readdirSync(problemsPath).map((problemName) => ({
+      title: toPascalCase(problemName),
+      filePath: path.join(
+        patternsRelativePath,
+        patternName,
+        problemName,
+        `${problemName}.js`
+      ),
+    }))
+
+    return {
+      title: toPascalCase(patternName),
+      problems,
+    }
+  })
+}
+
+const patternToMd = (pattern) => {
+  return `## ${pattern.title}\n\n${pattern.problems
+    .map((problem) => `* [${problem.title}](${problem.filePath})`)
+    .join("\n")}\n`
+}
+
+const clearAndUpper = (text) => text.replace(/-/, " ").toUpperCase()
+const toPascalCase = (text) => text.replace(/(^\w|-\w)/g, clearAndUpper)
+
+const generateReadme = () => {
+  const dirPath = path.resolve(__dirname)
+  const readmePath = path.join(dirPath, "../README.md")
+
+  fs.writeFileSync(
+    readmePath,
+    `![main workflow](https://github.com/teimurjan/educative-grokking-coding-interview/actions/workflows/main.yml/badge.svg)
+
+# About
+
+This repository contains solutions for [Grokking the Coding Interview: Patterns for Coding Question](https://www.educative.io/courses/grokking-the-coding-interview) from https://educative.io.
+
+# Get started
+
+## Install dependencies
+
+\`\`\`sh
+npm install
+\`\`\`
+
+## Run tests
+
+\`\`\`sh
+npm test
+\`\`\`
+
+# Patterns
+
+${getPatterns().map(patternToMd).join("\n")}
+  `
+  )
+}
+
+generateReadme()
